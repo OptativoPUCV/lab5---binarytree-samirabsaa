d3df3f92466d626e578923d1d2a9ae8999471231
Replit user <>
2023-04-22 00:59:11 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -62,7 +62,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     while(tree->current!=NULL){ 
       //caso de existir y ser igual, se puede sobreescribir o hacer nada
       if(is_equal(tree, key, tree->current->pair->key)){
-        tree->current->pair->value = value; 
+        //tree->current->pair->value = value; 
         return; 
       }
       
